dcdfaa459b7880462df552eb54ed4a71d5bd300f
Shanmugarajeshwaran R <shanmugarajeshwaran@sparkouttech.com>
2022-05-08 16:09:14 +0530

    Keyboard avoidance added

--- Custom-ActionSheet-SwiftUI/HomeContentView.swift
+++ Custom-ActionSheet-SwiftUI/HomeContentView.swift
@@ -20,13 +20,13 @@ struct HomeContentView: View {
             }
             VStack {
                 Spacer()
-                CustomActionSheet()
-                    .offset(y: self.showActionSheet ? 0 : UIScreen.main.bounds.height)
+                CustomActionSheet(showActionSheet: $showActionSheet)
             }
             .background(
                 (self.showActionSheet ? Color.black.opacity(0.3) : Color.clear)
                     .edgesIgnoringSafeArea(.all)
                     .onTapGesture {
+                        hideKeyboard()
                         self.showActionSheet.toggle()
                     }
             )
@@ -38,34 +38,43 @@ struct HomeContentView: View {
 }
 
 struct CustomActionSheet: View {
+    let showActionSheet: Binding<Bool>
+    @State var username: String = ""
+    @State var keyboardHeight: CGFloat = 0.0
     var body: some View {
         let bottomPadding = UIApplication.shared.windows.last?.safeAreaInsets.bottom ?? 0
         VStack(spacing: 15) {
-            HStack {
-                Text("Item 1")
-                Spacer()
-            }
-            HStack {
-                Text("Item 2")
-                Spacer()
-            }
-            HStack {
-                Text("Item 3")
-                Spacer()
-            }
-            HStack {
-                Text("Item 4")
-                Spacer()
-            }
-            HStack {
-                Text("Item 5")
-                Spacer()
-            }
+            Text("Item 1")
+            Text("Item 1")
+            Text("Item 1")
+            Text("Item 1")
+            TextField("User name", text: $username)
         }
+        .frame(maxWidth: .infinity)
         .padding(.bottom, bottomPadding + 10)
         .padding(.horizontal)
         .padding(.top, 20)
         .background(Color.white)
         .cornerRadius(15)
+        .onAppear {
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notification in
+                if let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+                    keyboardHeight = value.height
+                }
+            }
+            
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { notification in
+                keyboardHeight = 0
+            }
+        }
+        .offset(y: showActionSheet.wrappedValue ? -keyboardHeight : UIScreen.main.bounds.height)
     }
 }
+
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
